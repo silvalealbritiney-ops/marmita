9872a8f113e4617623dfe59308b71d75b83da988
silvalealbritiney-ops <silvalealbritiney@gmail.com>
2026-08-13 07:43:52 -0300

    Adicionado gerador de imagem do cardapio para redes sociais

--- marmitas.py
+++ marmitas.py
@@ -10,8 +10,8 @@ DATA_FILE = 'cardapio.json'
 def carregar_cardapio():
     if not os.path.exists(DATA_FILE):
         dados_padrao = {
-            "chave_pix": "28999999999",
-            "whatsapp": "5528999999999",
+            "chave_pix": "03178142738 - Edeildo",
+            "whatsapp": "+55 28 98815-8678",
             "especiais": {
                 "ativo": True,
                 "preco_m": "22,00",
